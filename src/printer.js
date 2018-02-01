aa21007ab5587fbbfacd0076300fc4cec037a165
Tim Seckinger <seckinger.tim@gmail.com>
2018-02-01 22:00:02 +0100

    print empty lines

--- src/printer.js
+++ src/printer.js
@@ -5,6 +5,7 @@ const print = tokens => {
   let code = '';
   let nestingLevel = 0;
   let prevAllowsSpace = false;
+  let consecutiveBreaks = 2;
 
   for (const { type, value } of tokens) {
     if (prevAllowsSpace && allowsSpaceBefore(type)) code += ' ';
@@ -16,10 +17,20 @@ const print = tokens => {
     prevAllowsSpace = allowsSpaceAfter(type);
 
     // hard line break
+    if (type === 'emptyLine') {
+      while (consecutiveBreaks < 2) {
+        code += '\n';
+        consecutiveBreaks++;
+        prevAllowsSpace = false; // line break replaces space
+      }
+    } else {
+      consecutiveBreaks = 0;
+    }
     if (type === 'leftPar') nestingLevel++;
     if (type === 'rightPar') {
       if (--nestingLevel === 0) {
         code += '\n';
+        consecutiveBreaks++;
         prevAllowsSpace = false; // line break replaces space
       }
     }
--- src/printer.test.js
+++ src/printer.test.js
@@ -24,3 +24,25 @@ test('inserts a space between tokens, but not inside of parens', () => {
     ]),
   ).toEqual('(= if abc)\n');
 });
+
+test('inserts an empty line if such a token occurs', () => {
+  expect(
+    print([
+      { type: 'leftPar', value: '(' },
+      { type: 'emptyLine', value: '' },
+      { type: 'rightPar', value: ')' },
+    ]),
+  ).toEqual('(\n\n)\n');
+});
+
+test('inserts only one empty line even if it follows a hard line break', () => {
+  expect(
+    print([
+      { type: 'leftPar', value: '(' },
+      { type: 'rightPar', value: ')' },
+      { type: 'emptyLine', value: '' },
+      { type: 'leftPar', value: '(' },
+      { type: 'rightPar', value: ')' },
+    ]),
+  ).toEqual('()\n\n()\n');
+});
